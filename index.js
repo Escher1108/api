606e8d78d991cef43ebc1a070398194a9fdd7e07
Escher1108 <3095697814@qq.com>
2022-03-15 21:59:07 +0800

    ecn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: Escher1108
+ * @Website: https://jkgblog.com
+ * @Date: 2022-03-05 21:42:49
+ * @LastEditTime: 2022-03-15 21:56:50
+ * @LastEditors: Do not edit
+ * @FilePath: \api\index.js
+ * @Description: 耶斯莫拉
+ */
 const express = require('express')
 const axios = require('axios')
 const { response } = require('express')
@@ -7,6 +16,8 @@ const app = express()
 const PORT = process.env.PORT || 5655
 
 app.use(express.json())
+
+const base64Arr =['aHR0cHM6Ly9ocy5qa2dibG9nLmNvbS9hcGkucGhw','aHR0cHM6Ly9yZXN0YXBpLmFtYXAuY29tL3YzL2dlb2NvZGUvcmVnZW8='];
 
 app.get('/', (req, res) => {
     res.send({ status: 'API is running', Author: 'Escher1108', website: 'https://jkgblog.com', Email: 'contact@jsproxy.cyou' })
@@ -24,7 +35,10 @@ app.post('/message', (req, res) => {
         text: result.text,
         desp: `基本数据：${result.desp}`
     }
-    axios.post('https://hs.jkgblog.com/api.php', data).then(response => {
+    
+    const buff_one = Buffer.from(base64Arr[0], 'base64');
+    const url1 = buff_one.toString('utf-8');
+    axios.post(url1, data).then(response => {
         return res.send(response.data)
     }).catch(err => {
         return res.send(response.data)
@@ -48,8 +62,9 @@ app.post('/gps', (req, res) => {
         , "Access-Control-Allow-Credentials": "true"
     });
 
-    let url = 'https://restapi.amap.com/v3/geocode/regeo'
-    axios.get(url, { params: gpsInfo }).then(async (response) => {
+    const buff_two = Buffer.from(base64Arr[1], 'base64');
+    const url2 = buff_two.toString('utf-8');
+    axios.get(url2, { params: gpsInfo }).then(async (response) => {
         let dataNum = await response.data
         return res.json(dataNum)
     }).catch(err => {
